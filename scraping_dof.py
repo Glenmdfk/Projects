15cea277066eb0f2e4518d741ba6e10cc7492195
Jose Mendoza <5672mendoza@gmail.com>
2024-09-11 12:12:30 -0600

    Se separa por clases y se agrega la función de generar archivos de excel desde SQLite

--- scraping_dof.py
+++ scraping_dof.py
@@ -1,7 +1,7 @@
 # if you wanna running this code you need to install openpyxl, lxml, html5lib
 # libraries
 from datetime import datetime as dt
-import inspect, warnings, logging
+import inspect, warnings
 from time import sleep
 import pandas as pd
 # selenium
@@ -14,10 +14,11 @@ import Libs.db_conn as db_conn
 # global variables and config
 default_download = 'C:\\App\\Download'
 file_generated = 'C:/App/Download/DOF.xlsx'
+dict_month = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio'
+    ,7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
 #configuration
 warnings.filterwarnings("ignore")
-logging.basicConfig(level=logging.ERROR)
-#DEBUG, INFO, WARNING, ERROR, CRITICAL
+# logging.basicConfig(level=logging.ERROR) #DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 # start definition
 class scraping:
@@ -25,10 +26,13 @@ class scraping:
     def __init__(self):
         self.trans = 0
         self.error = 0
+    # transaction handling
+    def handle_trans(self, msg):
+        bs.time_trans(msg)
+        self.trans += 1
     # error handling
     def handle_error(self, msg, error):
         bs.time_trans(msg, error)
-        logging.error('Error on : ' + error)
         self.error += 1
     # variable definition
     def variables(self):
@@ -36,8 +40,7 @@ class scraping:
             url = 'https://dof.gob.mx/'
             initial_date = bs.vars_date()[4]
             end_date = bs.vars_date()[1]
-            bs.time_trans('variables has been definied')
-            self.trans += 1
+            self.handle_trans('variables has been definied')
             return url, initial_date, end_date
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
@@ -53,7 +56,7 @@ class scraping:
             #Agregar a la lista de directorios
             path_list.append(path_download)
             path_list.append(path_processed)
-            self.trans += 1
+            self.handle_trans('Los directiorios fueron validados')
             return path_list
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
@@ -90,8 +93,7 @@ class scraping:
             
             # end of running
             driver.quit()
-            self.trans += 1
-            bs.time_trans('Scraping has ended')
+            self.handle_trans('Scraping has ended')
             return content
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
@@ -102,18 +104,48 @@ class scraping:
             df_list = pd.read_html(content, header=0)
             df_dof = df_list[0]
             df_dof.to_excel(file_generated, index = False)
-            bs.time_trans('File has been generated')
-            self.trans += 1
+            self.handle_trans('File has been generated')
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
             self.handle_error(msg, error)
+    # run all function
+    def load(self, proc):
+        try:
+            path_list = scraping.path_val(self)
+            bs.path_validator(path_list)
+            url, initial_date, end_date = scraping.variables(self)
+            content = scraping.new_scraping(self, url, initial_date, end_date)
+            scraping.render_table(self, content)
+        except Exception as error:
+            status = 'error'
+            msg_proc = 'Caugth this error: ' + repr(error)
+        else:
+            status = 'ok'
+            msg_proc = 'Nothing went wrong'
+        finally:
+            print('The class ' + proc + ' is finished.')
+            print('transacciones: ' + str(self.trans) + ', errores: ' + str(self.error))
+            return status, msg_proc
+
+class sqlite_load:
+    # initial count
+    def __init__(self):
+        self.trans = 0
+        self.error = 0
+    # transaction handling
+    def handle_trans(self, msg):
+        bs.time_trans(msg)
+        self.trans += 1
+    # error handling
+    def handle_error(self, msg, error):
+        bs.time_trans(msg, error)
+        self.error += 1
     # read excel file
     def transform_excel(self):
         try:
             name_cols = ['fecha','precio']
             df_to_load = pd.read_excel('C:/App/Download/DOF.xlsx', names=name_cols, header=0)
-            bs.time_trans('File is ready to load')
-            self.trans += 1
+            self.handle_trans('File is ready to load')
             return df_to_load
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
@@ -127,8 +159,7 @@ class scraping:
                 WHERE fecha BETWEEN '""" + str(date_ini) + """' AND '""" + str(date_end) + """';"""
             cursor.execute(query)
             conn.commit()
-            bs.time_trans('gob.dollar_by_date has been cleaned from ' + str(date_ini) + ' to ' + str(date_end))
-            self.trans += 1
+            self.handle_trans('gob.dollar_by_date has been cleaned from ' + str(date_ini) + ' to ' + str(date_end))
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
             self.handle_error(msg, error)
@@ -137,8 +168,7 @@ class scraping:
         try:
             table = 'gob.dollar_by_date'
             df_to_load.to_sql(table, engine, index=False, if_exists='append')
-            bs.time_trans(table + ' has been updated')
-            self.trans += 1
+            self.handle_trans(table + ' has been updated')
         except Exception as error:
             msg = 'Error on ' + inspect.currentframe().f_code.co_name
             self.handle_error(msg, error)
@@ -146,14 +176,10 @@ class scraping:
     def load(self, proc):
         try:
             engine, conn, cursor = db_conn.sqlite_with_alchemy()
-            path_list = scraping.path_val(self)
-            bs.path_validator(path_list)
             url, initial_date, end_date = scraping.variables(self)
-            content = scraping.new_scraping(self, url, initial_date, end_date)
-            scraping.render_table(self, content)
-            df_to_load = scraping.transform_excel(self)
-            scraping.clean_sql(self, initial_date, end_date, conn, cursor)
-            scraping.load_to_sql(self, df_to_load, engine)
+            df_to_load = sqlite_load.transform_excel(self)
+            sqlite_load.clean_sql(self, initial_date, end_date, conn, cursor)
+            sqlite_load.load_to_sql(self, df_to_load, engine)
             conn.close()
         except Exception as error:
             status = 'error'
@@ -162,12 +188,109 @@ class scraping:
             status = 'ok'
             msg_proc = 'Nothing went wrong'
         finally:
+            print('The class ' + proc + ' is finished.')
             print('transacciones: ' + str(self.trans) + ', errores: ' + str(self.error))
             return status, msg_proc
 
+class generate_excel:
+    # initial count
+    def __init__(self):
+        self.trans = 0
+        self.error = 0
+    # transaction handling
+    def handle_trans(self, msg):
+        bs.time_trans(msg)
+        self.trans += 1
+    # error handling
+    def handle_error(self, msg, error):
+        bs.time_trans(msg, error)
+        self.error += 1
+    # read sql table
+    def read_sql(self, engine):
+        try:
+            query = 'SELECT* FROM "gob.dollar_by_date"'
+            df_raw = pd.read_sql(query, engine)
+            df_dollar = df_raw.rename(columns={'fecha': 'fecha_raw', 'precio': 'precio'})
+            df_dollar['fecha'] = pd.to_datetime(df_dollar['fecha_raw'], format='%d-%m-%Y')
+            df_dollar['año'] = df_dollar['fecha'].dt.year
+            df_dollar['mes'] = df_dollar['fecha'].dt.month
+            df_dollar['mes_nv'] = df_dollar['fecha'].dt.month.apply(lambda x: f"{x:02d}")
+            df_dollar['nombre_mes'] = df_dollar['mes'].map(dict_month)
+            df_dollar['año_mes'] = df_dollar['nombre_mes'] + ' ' + df_dollar['año'].astype(str)
+            df_dollar['año_mes_filtro'] = df_dollar['año'].astype(str) + df_dollar['mes_nv'].astype(str) + '_' + df_dollar['nombre_mes']
+            self.handle_trans('SQL table is ready to process')
+            return df_dollar
+        except Exception as error:
+            msg = 'Error on ' + inspect.currentframe().f_code.co_name
+            self.handle_error(msg, error)
+    # define the file names
+    def file_names(self, df_dollar):
+        try:
+            list_month = df_dollar['año_mes_filtro'].unique().tolist()
+            path_processed = 'C:/App/Processed/'
+            dict_files = {}
+            for month in list_month:
+                name_file = path_processed + month + '.xlsx'
+                dict_files.update({month:name_file})
+            self.handle_trans('Lists has been generated')
+            return dict_files
+        except Exception as error:
+            msg = 'Error on ' + inspect.currentframe().f_code.co_name
+            self.handle_error(msg, error)
+    # filter and generate excel files
+    def generate_files(self, dict_files, df_dollar):
+        try:
+            for month in dict_files:
+                df_filtered = df_dollar[['fecha','precio','nombre_mes','año','año_mes']][df_dollar['año_mes_filtro'] == month]
+                df_filtered.to_excel(dict_files[month], index = False, sheet_name = month)
+            self.handle_trans('Files in excel has been generated')
+        except Exception as error:
+            msg = 'Error on ' + inspect.currentframe().f_code.co_name
+            self.handle_error(msg, error)
+    # generate unified file
+    def generate_unified(self, dict_files, df_dollar):
+        try:
+            file_name = 'C:/App/Processed/dollar_unificado.xlsx'
+            unificadoo = pd.ExcelWriter(file_name)
+            with unificadoo as writer:
+                # genera el archivo resumido
+                df_unificado = df_dollar[['fecha','precio','nombre_mes','año','año_mes']]
+                df_unificado.to_excel(writer, index=False, sheet_name = 'Precio_dolar')
+                # genera una hoja para cada mes
+                for month in dict_files:
+                    df_filtered = df_dollar[['fecha','precio','nombre_mes','año','año_mes']][df_dollar['año_mes_filtro'] == month]
+                    df_filtered.to_excel(writer, index = False, sheet_name = month)
+            self.handle_trans('Unified file excel has been generated')
+        except Exception as error:
+            msg = 'Error on ' + inspect.currentframe().f_code.co_name
+            self.handle_error(msg, error)
+    # run all function
+    def load(self, proc):
+        try:
+            engine, conn, cursor = db_conn.sqlite_with_alchemy()
+            df_dollar = generate_excel.read_sql(self, engine)
+            conn.close()
+            dict_files = generate_excel.file_names(self, df_dollar)
+            generate_excel.generate_files(self, dict_files, df_dollar)
+            generate_excel.generate_unified(self, dict_files, df_dollar)
+        except Exception as error:
+            status = 'error'
+            msg_proc = 'Caugth this error: ' + repr(error)
+        else:
+            status = 'ok'
+            msg_proc = 'Nothing went wrong'
+        finally:
+            print('The class ' + proc + ' is finished.')
+            print('transacciones: ' + str(self.trans) + ', errores: ' + str(self.error))
+            return status, msg_proc
+
+
 # start execution
+dict_clases = {'scraping':scraping(), 'sqlite_load':sqlite_load(), 'generate_excel':generate_excel()}
 def main():
-    run_class = scraping()
-    run_process = 'scraping'
-    result = run_class.load(run_process)
+    for clase in dict_clases:
+        print('Starting to run ' + clase)
+        run_process = clase
+        run_class = dict_clases[clase]
+        result = run_class.load(run_process)
 main()
